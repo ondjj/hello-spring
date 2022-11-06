684d40fa7fd8b71c46326cde53546e09da351bb2
ondjj <jcjk0302@gmail.com>
2022-11-06 22:23:12 +0900

    JPA / AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +19,12 @@ public class MemberService {
     }
 
     //    회원 가입
+    @Transactional
     public Long join(Member member){
-        // 같은 이름이 있는 중복 회원 x
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+            // 같은 이름이 있는 중복 회원 x
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
